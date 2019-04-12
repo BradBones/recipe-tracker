3b9f4e7c065af8a698b935a5e51fe518e85cf652
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-12 13:12:18 -0700

    setting up webpack config

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num23 from './test';
 
-console.log(`I imported ${num23} from another module!`);
+console.log(`I imported ${num23} from another module called test.js!`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    // This is the entry point of our source code. The code we are working on in development.
     entry: './src/js/index.js',
+    // Output only gets saved in memory when we do 'npm run start' it does NOT save the output files to the dist folder. To save files to the dist folder after development use 'npm run build'.
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-    
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 };
